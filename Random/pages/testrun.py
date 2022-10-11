89dc9fa46b7b3f53c2ef7ae80eb28cd51c508399
wlyi1 <82428894+wlyi1@users.noreply.github.com>
2022-10-11 16:58:58 +0700

    Update testrun.py

--- Random/pages/testrun.py
+++ Random/pages/testrun.py
@@ -1,11 +1,26 @@
 import pandas as pd
 import sqlite3
 
-# Read sqlite query results into a pandas DataFrame
-con = sqlite3.connect("rd1.db")
-df = pd.read_sql_query("SELECT * from randomk1", con)
+st.markdown("# Run Query")
+sqlite_dbs = [file for file in os.listdir('.') if file.endswith('.db')]
+#db_filename = st.selectbox('DB Filename', sqlite_dbs)
+db_filename = 'rd1.db'
 
-# Verify that result of SQL query is stored in the dataframe
-st.write(df.head())
+query = st.text_area("SQL Query", height=100)
+conn = create_connection(db_filename)
 
-con.close()
+submitted = st.button('Run Query')
+
+if submitted:
+    try:
+        query = conn.execute(query)
+        cols = [column[0] for column in query.description]
+        results_df= pd.DataFrame.from_records(
+            data = query.fetchall(), 
+            columns = cols
+        )
+        st.dataframe(results_df)
+    except Exception as e:
+        st.write(e)
+
+
